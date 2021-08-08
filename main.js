5c6572ccbcb78a587df73db2eeab1459a4173c52
raj rana <raj.rana507@gmail.com>
2021-08-08 21:34:44 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    const aside = document.getElementById("aside")
+    aside.className = "aside aside-show" 
+}
+function hideMenu() { 
+    const aside = document.getElementById("aside")
+    aside.className = "aside"
+}
